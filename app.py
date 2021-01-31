04b79052f645795cc8748ed1329f4c1123b5ab32
Yigal W <Yigal@users.noreply.github.com>
2021-01-31 15:30:22 +0200

    added start telegram session

--- app.py
+++ app.py
@@ -29,6 +29,9 @@ async def status():
         'overall': 'ok'
     }
 
+@app.route('/start_telegram_strategy')
+async def start_telegram_strategy(parsed_data):
+    #TODO implement
 
 @app.route('/market/candles/<symbol>')
 async def symbol_candles(symbol: str):
