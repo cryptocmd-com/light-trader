5cae44ac4a7ccaa713f71ee5952744dec0bc8dcc
Yigal W <Yigal@users.noreply.github.com>
2021-02-15 17:04:01 +0200

    add validation

--- strategy_external_advice.py
+++ strategy_external_advice.py
@@ -1,7 +1,7 @@
 import logging
 from decimal import Decimal
 import strategy_base
-
+MAX_QUANTITY = 1000
 
 logger = logging.getLogger(__name__)
 
@@ -16,14 +16,26 @@ class StrategyExternalAdvice(strategy_base.StrategyBase):
         # TODO: Check arguments. If they're not satisfactory raise ValueError
         super().__init__(executor)
         self.entry_quantity = Decimal('0.01')
-        self.take_profit_price = Decimal(advice.get('TP1', '35000'))
-        self.stop_loss_price = Decimal(advice.get('SL1', '28000'))
+        self.take_profit_price = Decimal(advice.get('SL1'))
+        self.stop_loss_price = Decimal(advice.get('TP1'))
+
+
+    def validate_external_command(self):
+        if (not self.entry_quantity > 0):
+                       logger.error(
+                "Ignored trade-plan, non positive quantaty: %s",
+                repr(self.entry_quantity))
+        if (not self.entry_quantity < MAX_QUANTITY):
+                       logger.error(
+                "Ignored trade-plan, quantaty bellow max: %s",
+                repr(self.entry_quantity))
+
 
     def plan_trade(self, candle: dict):
         # To log messages use e.g. logger.info( ... )
-
         # If not trade is to be exdcuted, return None
         # Else, return an object as-per the example below
+        self.validate_external_command()
         return strategy_base.TradePlanAtUnspecifiedPrice(
             symbol=candle['symbol'],
             entry_quantity=self.entry_quantity,
