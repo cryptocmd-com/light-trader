b3c55514f5a86a07d36879cc227c57a2388e6ea8
Yitzik Casapu <yitzikc@gmail.com>
2021-02-22 03:57:27 +0000

    Improve logging of order fills

--- strategy_base.py
+++ strategy_base.py
@@ -37,29 +37,30 @@ class StrategyBase(
         order_id = response.get('clientOrderId')
         orig_qty = Decimal(response['origQty'])
         exec_qty = Decimal(response.get('executedQty', 0))
-        transact_time = response.get('transactTime')
+        transact_time = response.get('transactTime', 0)
+        side = response.get('side')
         status = response.get('status', 'UNKNOWN')
         if orig_qty == exec_qty:
             if status.upper() != 'FILLED':
                 raise ValueError(
                     f'Order {order_id} appears fully filled but status is {status}')
             logger.info(
-                'Order %s filled qty=%s at t=%d',
-                order_id, exec_qty, transact_time)
+                'Order %s filled %s %s at t=%d',
+                order_id, side, exec_qty, transact_time)
         elif exec_qty > 0:
             logger.info(
-                'Order %s partly filled %s/%s at t=%d. status: %s',
-                order_id, exec_qty, orig_qty, transact_time, status)
+                'Order %s partly filled %s %s/%s at t=%d. status: %s',
+                order_id, side, exec_qty, orig_qty, transact_time, status)
         else:
             logger.info(
-                'Order %s not filled at t=%d. status: %s',
-                order_id, transact_time, status)
+                'Order %s to %s %s not filled at t=%d. status: %s',
+                order_id, side, orig_qty, transact_time, status)
             return
 
         # TODO: Calculate average fill price and commission.
         position_increment_sign = {
             'BUY': 1,
             'SELL': -1
-        }[response['side']]
+        }[side]
         self.position += position_increment_sign * exec_qty
         return
